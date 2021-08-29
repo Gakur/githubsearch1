2b1963cf0b89362d73a393fe8970fc4391773e6b
Peter Gakure <petergakure97@gmail.com>
2021-08-29 14:31:00 +0300

    Create component githubsearch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GithubsearchComponent } from './githubsearch/githubsearch.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GithubsearchComponent
   ],
   imports: [
     BrowserModule,
