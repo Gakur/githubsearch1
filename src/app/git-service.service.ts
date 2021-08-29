daacb4937d92196db27a311b850a3dcdf44c6e02
Peter Gakure <petergakure97@gmail.com>
2021-08-29 21:48:17 +0300

    Update new file

--- src/app/git-service.service.ts
+++ src/app/git-service.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Repositories } from './repositories';
+import { User } from './user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GitServiceService {
+  private username!:string;
+  user!:User;
+  repos!:Repositories;
+  reposArray:any;
+  BaseUrl = "https://api.github.com/users/";
+  FinalUrl ="?client_id=63803c21ae36ca102d8f&client_secret=035207e594845697a248822218ac167e7735bb0b";
+  Api= "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"
+  ;
+  
 
-  constructor() { }
+  constructor() {
+    this.username = "Gakur";
+    this.user= new User ("","","",0,0,0,"");
+    this.repos = new Repositories("","","");
+    this.reposArray = [];
+  }
 }
