2abf64dc1b0e7f65dab0ce0d5e09ba8be3578283
Peter Gakure <petergakure97@gmail.com>
2021-08-29 16:42:26 +0300

    Update modified file

--- src/app/githubsearch/githubsearch.component.ts
+++ src/app/githubsearch/githubsearch.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../user';
 
 @Component({
   selector: 'app-githubsearch',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./githubsearch.component.css']
 })
 export class GithubsearchComponent implements OnInit {
+  user!: User;
+  public username!: string
 
   constructor() { }
 
