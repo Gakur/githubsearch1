05d1945e40a6b222f5e47ce08c260f051130071e
Peter Gakure <petergakure97@gmail.com>
2021-08-31 11:26:53 +0300

    Update modified file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GithubsearchComponent } from './githubsearch/githubsearch.component';
+import { AboutComponent } from './about/about.component';
+import { CommonModule } from '@angular/common';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:"githubsearch",component:GithubsearchComponent },
+  { path:"about", component: AboutComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)],
+
+  exports: [RouterModule],
+  declarations: []
 })
 export class AppRoutingModule { }
