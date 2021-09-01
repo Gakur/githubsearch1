dd8e721546e43d593629b9ed602d78db1e4006c6
Peter Gakure <petergakure97@gmail.com>
2021-09-01 15:12:39 +0300

    added environments to gitignore

--- src/app/git-service.service.ts
+++ src/app/git-service.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -14,10 +14,10 @@ export class GitServiceService {
   }
 
   userRequest(username: string) {
-    return this.http.get("https://api.github.com/users/" + username + '?access_token=' + environment.ApiKey)
+    return this.http.get("https://api.github.com/users/" + username + '?access_token=' + environment.accessToken)
   }
   repositoryRequest(username: string) {
-    return this.http.get('https://api.github.com/users/' + username + '/repos?access_token=' + environment.ApiKey)
+    return this.http.get('https://api.github.com/users/' + username + '/repos?access_token=' + environment.accessToken)
   }
 }
 
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production:false,
+  accessToken:"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"
+}
